619bccb7c601f4516dfd0442d5d44b7055070e3d
Putbotz <96361236+Putbotz@users.noreply.github.com>
2022-02-07 19:57:21 +0800

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -2,10 +2,10 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
 *────────「 SCRIPT 」 ────────*
 Hai 👋
-saya fahrilbotz
+saya putbotz
 Bot Ini Menggunakan Sc dari github
 -
-`.trim(), '© fahrilbotz', 'Thanks', '.tqto')
+`.trim(), '© putbotz', 'Thanks', '.tqto')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
